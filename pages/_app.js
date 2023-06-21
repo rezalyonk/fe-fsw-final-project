2e2acd752a8b21d9cff456375ca8a15e2393db37
Reza Lyon Kurniawan <rezalyonk01@gmail.com>
2023-06-21 20:25:15 +0700

    menambahkan import boostrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
 import Navbar from '../components/Navbar'
 import '@/styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-function MyApp ({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return (
-  <>
-    <Component {...pageProps} />
+    <>
+      <Component {...pageProps} />
     </>
   )
 }
