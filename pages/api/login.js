2aa2b117eca1a29866fdbf1746a693059f6e7823
Fahrur Rozi <rfahrur6054@gmail.com>
2023-06-24 14:11:26 +0700

    logic login (belum tampilan)

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,22 +1,40 @@
 import axios from 'axios';
-import Cookies from 'js-cookie';
+import { serialize } from 'cookie';
 
-export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    try {
-      const response = await axios.post('https://be-fsw-final-project-production-55d6.up.railway.app/v1/api/login', req.body);
-      
-      // Set cookie with the access token
-      Cookies.set('access_token', response.data.access_token);
-      
-      // Proses response dari server API
-      // ...
-      res.status(200).json(response.data);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Internal Server Error' });
-    }
-  } else {
+export default async function login(req, res) {
+  if (req.method !== 'POST') {
     res.status(405).json({ message: 'Method Not Allowed' });
+    return;
+  }
+
+  const { email, password } = req.body;
+
+  try {
+    const response = await axios.post('https://be-fsw-final-project-production-55d6.up.railway.app/v1/api/login', {
+      email,
+      password,
+    });
+
+    const { data } = response;
+    console.log(data); // Periksa respon dari server
+    
+    if (data.success) {
+      res.setHeader('Set-Cookie', serialize('accessToken', data.accessToken, {
+        httpOnly: true,
+        path: '/',
+      }));
+      res.json({ message: 'Login berhasil' });
+    } else {
+      if (data.message === 'Email tidak terdaftar') {
+        res.status(400).json({ message: 'Email tidak terdaftar' });
+      } else if (data.message === 'Password salah') {
+        res.status(400).json({ message: 'Password salah' });
+      } else {
+        res.status(500).json({ message: 'Terjadi kesalahan saat login' });
+      }
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Terjadi kesalahan saat login' });
   }
 }
--- pages/login/index.js
+++ pages/login/index.js
@@ -1,18 +1,90 @@
-import FormLogin from "../../components/form-login";
-import styles from "./index.module.css";
-const Login = () => {
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [accessToken, setAccessToken] = useState('');
+
+  useEffect(() => {
+    // Periksa status login dan accessToken saat halaman dimuat
+    const storedAccessToken = localStorage.getItem('accessToken');
+
+    if (storedAccessToken) {
+      setLoggedIn(true);
+      setAccessToken(storedAccessToken);
+    }
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('/api/login', {
+        email,
+        password,
+      });
+
+      const { data } = response;
+      console.log(data)
+      if (data.message === 'Login berhasil') {
+        // Simpan status login dan accessToken di localStorage
+        localStorage.setItem('accessToken', data.accessToken);
+        setLoggedIn(true);
+        setAccessToken(data.accessToken);
+        console.log(setAccessToken)
+      } else {
+        setMessage(data.message);
+      }
+    } catch (error) {
+      console.error(error);
+      setMessage('Terjadi kesalahan saat login');
+    }
+  };
+
+  const handleLogout = () => {
+    const accessToken = localStorage.getItem('accessToken');
+  console.log(accessToken); // Periksa nilai accessToken sebelum dihapus
+    // Hapus status login dan accessToken dari localStorage saat logout
+    localStorage.removeItem('accessToken');
+    setLoggedIn(false);
+    setAccessToken('');
+  };
+
+
   return (
-    <div className={styles.login}>
-      <div className={styles.wLogin}>
-        <img
-          className={styles.gambargradasiIcon}
-          alt=""
-          src="/gambargradasi@2x.png"
-        />
-        <FormLogin />
-      </div>
+    <div>
+      {loggedIn ? (
+        <div>
+          <p>Logged in</p>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button type="submit">Login</button>
+          {message && <p>{message}</p>}
+        </form>
+      )}
     </div>
   );
-};
-
-export default Login;
+}
