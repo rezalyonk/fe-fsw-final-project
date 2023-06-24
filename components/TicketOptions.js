21905c735a7c18ceb43d04e522aeb15be06bea40
ariyog <ariyogi0022@gmail.com>
2023-06-24 22:45:52 +0700

    fetch all data Flight

--- components/TicketOptions.js
+++ components/TicketOptions.js
@@ -1,14 +1,28 @@
-import data from "../public/data.json";
 import styles from "../styles/pilih.module.css";
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
+import axios from 'axios';
 
 
 const Destinations = () => {
-  
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `https://be-fsw-final-project-production-55d6.up.railway.app/v1/api/home`
+        );
+        setData(response.data.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  });
   return (
     <div className={styles.destinationcontainer}>
       <div className={styles.destinationgrid}>
-        {data.data.map(destination => (
+        {data.map(destination => (
           <div key={destination.id} className={styles.destinationcard}>
             <img src={destination.foto} alt={destination.bandaraTujuan.nama_bandara} />
             <div className={styles.destinationinfo}>
