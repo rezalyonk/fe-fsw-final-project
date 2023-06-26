a5cbe3b5736af839800325d5d6894e19c06a00f3
Reza Lyon Kurniawan <rezalyonk01@gmail.com>
2023-06-26 19:25:38 +0700

    ada perubahan

--- components/form-Caripenerbangan.js
+++ components/form-Caripenerbangan.js
@@ -10,7 +10,6 @@ const berandaCariPenerbangan = () => {
                 <meta charset="UTF-8" />
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous" />
             </Head>
-            <h1 className={style.testh1}>melakukan testting</h1>
             <div className='container-fluid'>
                 <div className='container'>
                     <div className='row'>
