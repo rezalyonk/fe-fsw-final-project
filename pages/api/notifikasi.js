deca170018fbc9397bda9f43260ec797ad8fae20
Fahrur Rozi <rfahrur6054@gmail.com>
2023-07-03 23:44:57 +0700

    update notifikasi & pembayaran

--- pages/api/notifikasi.js
+++ pages/api/notifikasi.js
@@ -1,12 +1,23 @@
-import axios from 'axios';
+import axios from "axios";
 
 export default async (req, res) => {
   try {
-    const response = await axios.get('https://mang-eak-production.up.railway.app/v1/api/notification');
+    // Mendapatkan accessToken dari header Authorization
+    const accessToken = req.headers.authorization.replace("Bearer ", "");
+
+    // Mengirim permintaan ke endpoint dengan menyertakan accessToken sebagai header Authorization
+    const response = await axios.get(
+      "https://mang-eak-production.up.railway.app/v1/api/notification",
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
     const data = response.data;
 
     res.status(200).json(data);
   } catch (error) {
-    res.status(500).json({ error: 'Internal server error' });
+    res.status(500).json({ error: "Internal server error" });
   }
 };
